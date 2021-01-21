531adfce7b254dc43df06cb5eca0ba0f95e0b1d2
wenbin.yu <yuwenbin1061@hotmail.com>
2021-01-21 10:04:31 +0800

    Remove Powered by Metabase

--- frontend/src/metabase/public/components/LogoBadge.jsx
+++ frontend/src/metabase/public/components/LogoBadge.jsx
@@ -10,20 +10,21 @@ type Props = {
 };
 
 const LogoBadge = ({ dark }: Props) => (
-  <a
-    href="https://metabase.com/"
-    target="_blank"
-    className="h4 flex text-bold align-center no-decoration"
-  >
-    <LogoIcon height={28} dark={dark} />
-    <span className="text-small">
-      <span className="ml1 md-ml2 text-medium">{jt`Powered by ${(
-        <span className={dark ? "text-white" : "text-brand"}>
-          {t`Metabase`}
-        </span>
-      )}`}</span>
-    </span>
-  </a>
+  <div></div>
+  // <a
+  //   href="https://metabase.com/"
+  //   target="_blank"
+  //   className="h4 flex text-bold align-center no-decoration"
+  // >
+  //   <LogoIcon height={28} dark={dark} />
+  //   <span className="text-small">
+  //     <span className="ml1 md-ml2 text-medium">{jt`Powered by ${(
+  //       <span className={dark ? "text-white" : "text-brand"}>
+  //         {t`Metabase`}
+  //       </span>
+  //     )}`}</span>
+  //   </span>
+  // </a>
 );
 
 export default LogoBadge;
